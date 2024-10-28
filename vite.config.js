96fb87122ef7f8583ec9cc3ae30e3554f023bc78
sadcparliament <sadcparliament@gmail.com>
2024-10-28 14:10:02 +0000

    CodebaseAdminTheme

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/main.scss',
+                'resources/sass/codebase/themes/corporate.scss',
+                'resources/sass/codebase/themes/earth.scss',
+                'resources/sass/codebase/themes/elegance.scss',
+                'resources/sass/codebase/themes/flat.scss',
+                'resources/sass/codebase/themes/pulse.scss',
+                'resources/js/codebase/app.js',
+                'resources/js/app.js',
+                'resources/js/pages/datatables.js',
+            ],
             refresh: true,
         }),
     ],
